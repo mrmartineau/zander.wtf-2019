7d9bc49dbdfc7547b00ace680a1c6501979d3852
Zander Martineau <zander.martineau@nimbletank.com>
2018-01-25 11:55:09 +0000

    fixes API endpoint

--- utils/prismic.js
+++ utils/prismic.js
@@ -1,7 +1,7 @@
 import Prismic from 'prismic-javascript'
 const PRISMIC_ACCESS_TOKEN =
   'MC5XbVNqWkNrQUFBUUo1anA2.77-977-9AO-_ve-_ve-_ve-_vSBicDzvv73vv73vv73vv73vv70G77-9GO-_ve-_vWXvv71Q77-977-9cO-_ve-_ve-_ve-_vUE'
-const PRISMIC_REPO = 'http://zanderwtf.prismic.io/api/v2'
+const PRISMIC_REPO = 'https://zanderwtf.prismic.io/api/v2'
 
 export const initApi = req => {
   return Prismic.getApi(PRISMIC_REPO, {
