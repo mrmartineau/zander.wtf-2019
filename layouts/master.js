5b1cb773bb546815e788c77c95fdb70e3214ea59
Zander Martineau <zander@martineau.tv>
2018-02-17 22:41:23 +0000

    add favicons

--- layouts/master.js
+++ layouts/master.js
@@ -29,6 +29,27 @@ export default ({
       <link rel="preconnect" href="https://pinboard-api.now.sh" />
       <link rel="preconnect" href="https://zanderwtf.prismic.io" />
       {canonical && <link rel="canonical" href={canonical} />}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/static/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#000000" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#000000" />
     </Head>
 
     {children}
