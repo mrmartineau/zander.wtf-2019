30c3b02f0179d3fc4e711566eb42210c1404b9b9
Zander Martineau <zander@martineau.tv>
2018-02-25 00:09:38 +0000

    remove `intro` option from container to spaced

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -4,10 +4,10 @@ import { ds } from '../../designsystem'
 export const Container = styled.div`
   width: 90%;
   max-width: ${ds.get('layout.maxWidth')};
-  margin: ${props => props.intro ? '20vw auto' : '0 auto'};
+  margin: 0 auto;
 `
 
 export const Spacer = styled.div`
-  margin-top: 10vw;
-  margin-bottom: 10vw;
+  margin-top: ${props => props.intro ? '20vw' : '10vw'};
+  margin-bottom: ${props => props.intro ? '20vw' : '10vw'};
 `
