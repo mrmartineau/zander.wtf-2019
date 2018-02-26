8e3e0f9128b73ac285c59a8d01912fb7f6078528
Zander Martineau <zander@martineau.tv>
2018-02-26 19:02:54 +0000

    move `Container` & `Spacer` into own files

--- components/WorkFeed.js
+++ components/WorkFeed.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
-import { Container } from './common/Layout'
+import Container from './Container'
 import {
   FeedWrapper,
   FeedList,
