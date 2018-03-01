233526138477ae1b4f053424950bbebe96da1822
Zander Martineau <zander@martineau.tv>
2018-03-01 16:51:38 +0000

    bump next-offline to fix serviceworkers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
 const withOffline = require('next-offline')
 
 module.exports = withOffline()
+
+// {
+//   workboxOpts: {
+//     runtimeCaching: [
+//       { urlPattern: /^https?.*/, handler: 'networkFirst' },
+//       {
+//         urlPattern: /\.(?:png|gif|jpg|jpeg|svg)$/,
+//         handler: 'cacheFirst',
+//       },
+//       {
+//         urlPattern: 'https://zanderwtf.cdn.prismic.io/api/*',
+//         handler: 'staleWhileRevalidate',
+//         options: {
+//           cacheableResponse: {
+//             statuses: [0, 200],
+//           },
+//         },
+//       },
+//     ],
+//   },
+// }
+
