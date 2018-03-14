7750238112ccba25379285b9c72adff8494a52d5
Zander Martineau <zander.martineau@nimbletank.com>
2018-03-14 14:15:08 +0000

    work feed updates

--- components/WorkFeed.js
+++ components/WorkFeed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
+import { ds } from '../designsystem'
 import Container from './Container'
 import {
   FeedWrapper,
@@ -13,35 +14,46 @@ import {
   FeedItemBox,
   FeedItemLinkUrl,
 } from './Feed'
-import { ds } from '../designsystem'
 
 export const WorkFeedItemBox = styled.div`
-  display: block;
+  position: relative;
+`
+
+const Title = styled.div`
+  position: relative;
+  font-weight: bold;
+  font-size: ${ds.fs(4)};
+  text-transform: uppercase;
+  line-height: 1;
 
   @media screen and (min-width: ${ds.bp('m')}) {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    font-size: ${ds.fs(5)};
   }
 `
 
 export const WorkFeedItemImageWrapper = styled.div`
-  margin-top: ${ds.space(2)};
-  width: 50%;
-  /* margin-left: auto; */
-  /* margin-right: auto; */
+  position: absolute;
+  top: ${ds.space(2)};
+  right: 0;
+  width: 40%;
+  opacity: 0.6;
 
   @media screen and (min-width: ${ds.bp('m')}) {
-    margin-left: ${ds.space(2)};
-    margin-top: 0;
-    flex-basis: 27%;
-    flex-shrink: 0;
+    opacity: 1;
   }
 `
 
 export const WorkFeedItemImage = styled.img`
   display: block;
   max-width: 100%;
+  margin-left: auto;
+`
+const WorkCopyWrapper = styled.div`
+  ${'' /* width: 80%; */}
+
+  @media screen and (min-width: ${ds.bp('m')}) {
+    width: 60%;
+  }
 `
 
 export default props => {
@@ -50,15 +62,13 @@ export default props => {
     const title = item.data.title[0].text
     return (
       <FeedItem key={`work-${index}`}>
-        <FeedItemLink href={linkUrl} title={title} target="_blank" rel="noopener">
+        <FeedItemLink
+          href={linkUrl}
+          title={title}
+          target="_blank"
+          rel="noopener"
+        >
           <WorkFeedItemBox>
-            <div>
-              <FeedItemLinkTitle>{title}</FeedItemLinkTitle>
-              {item.data.description.length > 0 && (
-                <FeedItemDesc>{item.data.description[0].text}</FeedItemDesc>
-              )}
-              {linkUrl && <FeedItemLinkUrl>{linkUrl}</FeedItemLinkUrl>}
-            </div>
             {item.data.image.url && (
               <WorkFeedItemImageWrapper>
                 <WorkFeedItemImage
@@ -67,6 +77,13 @@ export default props => {
                 />
               </WorkFeedItemImageWrapper>
             )}
+            <Title>{title}</Title>
+            <WorkCopyWrapper>
+              {item.data.description.length > 0 && (
+                <FeedItemDesc>{item.data.description[0].text}</FeedItemDesc>
+              )}
+              {linkUrl && <FeedItemLinkUrl>{linkUrl}</FeedItemLinkUrl>}
+            </WorkCopyWrapper>
           </WorkFeedItemBox>
         </FeedItemLink>
       </FeedItem>
