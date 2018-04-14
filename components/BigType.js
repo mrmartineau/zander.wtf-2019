cf43984a9512e9127401b4a1a432b76acd0d53d1
Zander Martineau <zander@martineau.tv>
2018-04-15 00:43:40 +0100

    add smooth scrolling with dynamic speed based on `scrollY`

--- components/BigType.js
+++ components/BigType.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import ScrollObserver from 'scroll-observer'
+import jump from 'jump.js'
 import { ds } from '../designsystem/index'
 import { paddedLinkStyles } from '../designsystem/globalStyles'
 
@@ -51,13 +52,27 @@ const name = ['Z', 'A', 'N', 'D', 'E', 'R']
 export default class extends Component {
   componentDidMount() {
     if (this.bigType !== null) {
-      const bigTypeHeight = this.bigType.clientHeight
-      const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-      const windowHeightHalfed = windowHeight / 2
-      const threshold = bigTypeHeight - windowHeight - windowHeightHalfed
+      this.bigTypeHeight = this.bigType.clientHeight
+      this.windowHeight = Math.max(
+        document.documentElement.clientHeight,
+        window.innerHeight || 0
+      )
+      const windowHeightHalfed = this.windowHeight / 2
+      const threshold = this.bigTypeHeight - this.windowHeight - windowHeightHalfed
       this.scrollObserver = new ScrollObserver(this.skippy, {
         threshold: threshold,
         classNameActive: 'is-inactive',
+      })
+    }
+  }
+
+  jumpClick() {
+    if (this.bigType !== null) {
+      const initialDuration = 5000
+      const percentageScrolled = (window.scrollY / (this.bigTypeHeight - this.windowHeight))
+      const duration = parseInt(initialDuration - (initialDuration * percentageScrolled), 10)
+      jump('#main', {
+        duration: duration
       })
     }
   }
@@ -65,7 +80,11 @@ export default class extends Component {
     return (
       <BigType innerRef={bigType => (this.bigType = bigType)}>
         {name.map(item => <BigTypeItem key={item}>{item}</BigTypeItem>)}
-        <SkipLink href="#main" innerRef={skippy => (this.skippy = skippy)}>
+        <SkipLink
+          href="#main"
+          innerRef={skippy => (this.skippy = skippy)}
+          onClick={this.jumpClick.bind(this)}
+        >
           SKIP
         </SkipLink>
       </BigType>
