a17199c2c1ca27cf9750893d512dfdbb402b3252
Zander Martineau <zander@martineau.tv>
2019-03-27 00:30:16 +0000

    fix up some HEAD info

--- pages/_document.js
+++ pages/_document.js
@@ -35,14 +35,28 @@ export default class MyDocument extends Document {
           />
           <meta name="geo.region" content="GB" />
           <meta name="geo.placename" content="London" />
-          <meta name="theme-color" content="#000" />
+          <meta name="theme-color" content="#161a1d" />
           <meta name="application-name" content="Zander" />
           <link rel="dns-prefetch" href="https://zanderwtf.prismic.io" />
           <link rel="dns-prefetch" href="https://pinboard-api.now.sh" />
           <link rel="preconnect" href="https://pinboard-api.now.sh" />
           <link rel="preconnect" href="https://zanderwtf.prismic.io" />
-          <link rel="prefetch" href="/static/fonts/ColfaxWebRegularSub.woff" />
-          <link rel="prefetch" href="/static/fonts/ColfaxWebBoldSub.woff" />
+          <link
+            rel="prefetch"
+            href="/static/fonts/iAWriterDuospace/iAWriterDuospace-Regular.woff2"
+          />
+          <link
+            rel="prefetch"
+            href="/static/fonts/iAWriterDuospace/iAWriterDuospace-Italic.woff2"
+          />
+          <link
+            rel="prefetch"
+            href="/static/fonts/iAWriterDuospace/iAWriterDuospace-Bold.woff2"
+          />
+          <link
+            rel="prefetch"
+            href="/static/fonts/iAWriterDuospace/iAWriterDuospace-BoldItalic.woff2"
+          />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -64,13 +78,13 @@ export default class MyDocument extends Document {
           <link
             rel="mask-icon"
             href="/static/favicon/safari-pinned-tab.svg"
-            color="#000000"
+            color="#161a1d"
           />
-          <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="msapplication-TileColor" content="#161a1d" />
+          <meta name="theme-color" content="#161a1d" />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@mrmartineau" />
-          <meta property="og:locale" content="en_US" />
+          <meta property="og:locale" content="en_GB" />
         </Head>
         <body>
           <Main />
