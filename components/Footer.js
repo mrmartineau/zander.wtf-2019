3890a9057b9e5affe4b4e5bf8f6a96dc14798e7f
Zander Martineau <hi@zander.wtf>
2019-09-08 14:11:36 +0100

    update the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
 import BigType from './BigType'
+import { Link } from './Link'
+import { links } from '../data/links'
 
-export const Footer = () => <BigType />
+const Info = styled.div`
+  margin: 3rem 0;
+  text-align: center;
+  line-height: 2;
+`
+
+export const Footer = () => (
+  <Fragment>
+    <Info>
+      &copy;{new Date().getFullYear()}&nbsp;/&nbsp;
+      {links.map((item, index) => (
+        <Fragment key={index}>
+          <Link href={item.href}>{item.copy}</Link>
+          {' / '}
+        </Fragment>
+      ))}
+      <Link href="/atom.xml">RSS</Link>
+    </Info>
+
+    <BigType />
+  </Fragment>
+)
